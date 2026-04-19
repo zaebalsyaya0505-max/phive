d2cac272f549ba8ff8ed022702c07e32a19b5ee8
zaebalsyaya0505-max <zaebalsyaya0505-max@github.com>
2026-04-19 13:59:44 +1100

    Add Vercel Speed Insights for performance monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AdvertisePage from './pages/AdvertisePage';
@@ -26,6 +27,7 @@ function App() {
         </Route>
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
